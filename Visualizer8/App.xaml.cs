b4aa3a7eb59ff0d27c54395f6238c3b81ca72da8
aybjax <aybjax@gmail.com>
2024-03-28 18:32:09 +0500

    firstCommit

---
+++ Visualizer8/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Visualizer8;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Visualizer8/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Visualizer8;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
